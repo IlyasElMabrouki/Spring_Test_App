a74369ba5b5eb4533f308369e9666492a2268641
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-01-31 23:17:30 +0100

    Added Student Model, Controller, Service classes

---
+++ src/main/java/com/ilyaselmabrouki/demo/Student/StudentService.java
@@ -0,0 +1,16 @@
+package com.ilyaselmabrouki.demo.Student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(1L , "Ilyas", "ilyas@gmail.com", LocalDate.of(2002, Month.JUNE, 27), 21)
+        );
+    }
+}
